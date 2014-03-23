89a993e0d29ba5bb6e69fec3abcb2db7adbc497a
Natali Simeonova <simeonova.natali@gmail.com>
2014-03-23 11:51:26 +0200

    init

---
+++ 03.OperatorsExpressionsAndVariables/03.DivideBy7And5/Division.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Division
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your number:");
+        int num = int.Parse(Console.ReadLine());
+        bool result = false;
+        if ((num%5==0)&&(num%7==0))
+        {
+            result = true;   
+        }
+         if (num == 0)
+        {
+            result = false;
+        }
+        else
+        {
+            result = false;
+        }
+        Console.WriteLine(result);
+
+    }
+}
+
